f22bea38824bc42987574a36331c5e8c05c52f47
Kunwar Aridaman Singh <aridaman90151@gmail.com>
2023-07-27 11:36:25 +0530

    Workflows

---
+++ src/text_Summariser/constants/__init__.py
@@ -0,0 +1,6 @@
+# we are going to store the constant file paths here  ie.e config.yaml and params.yaml
+
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path("config/config.yaml")
+PARAMS_FILE_PATH = Path("params.yaml")
